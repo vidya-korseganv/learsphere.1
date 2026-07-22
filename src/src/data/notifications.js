f0aa992fa24b732662ed3e618b59932368f413a4
vidya-korseganv <vidyakorseganv24@gmail.com>
2026-07-22 15:18:42 +0530

    add data

--- src/src/data/notifications.js
+++ src/src/data/notifications.js
@@ -4,6 +4,7 @@ const notifications = [
   { id: 3, title: 'New cohort starting soon', time: '3d', unread: false },
   { id: 4, title: 'Update: Course material refreshed', time: '5d', unread: false },
   { id: 5, title: 'Invite: Join the design workshop', time: '1w', unread: true },
+  { id: 6, title: "Reminder: Complete your profile", time: '2w', unread: false },
 ]
 
 export default notifications
